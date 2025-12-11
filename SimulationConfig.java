b2b6df4ce47b93e231d10fd3075e7c72a3c6b9f3
Mennawalid255 <168589636+Mennawalid255@users.noreply.github.com>
2025-12-12 00:48:43 +0200

    Refactor SimulationConfig constructor
    Removed unnecessary comments and TODOs from the constructor.

--- SimulationConfig.java
+++ SimulationConfig.java
@@ -1,13 +1,9 @@
 
 public class SimulationConfig {
 
-    /**
-     * @param n
-     * @param j
-     * @param k
-     */
     public SimulationConfig(long n, int j, int k) {
-        //TODO Auto-generated constructor stub
+
     }
 
 }
+
