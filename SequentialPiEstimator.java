2d4a1e3d99390df7e63be00ebb149698bf7eaf89
Mennawalid255 <168589636+Mennawalid255@users.noreply.github.com>
2025-12-12 00:48:09 +0200

    Implement estimate method in SequentialPiEstimator
    Implemented the estimate method in SequentialPiEstimator.

--- SequentialPiEstimator.java
+++ SequentialPiEstimator.java
@@ -2,8 +2,8 @@
 public class SequentialPiEstimator {
 
     public double estimate(SimulationConfig config) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'estimate'");
+
     }
 
 }
+
