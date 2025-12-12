63c495d0ce5e5556e9b88dbd60d112151d973b5c
Shamss05 <shams.hesham05@gmail.com>
2025-12-12 02:21:32 +0200

    SimulationConfig class

--- Classes/SimulationConfig.java
+++ Classes/SimulationConfig.java
@@ -1,8 +1,40 @@
-
 public class SimulationConfig {
 
-    public SimulationConfig(long n, int j, int k) {
-        //TODO Auto-generated constructor stub
+    private final long totalPoints;   
+    private final int numTasks;       
+    private final int numThreads;     
+
+    public SimulationConfig(long totalPoints, int numTasks, int numThreads) {
+        if (totalPoints <= 0)
+            throw new IllegalArgumentException("totalPoints must be > 0");
+        if (numTasks <= 0)
+            throw new IllegalArgumentException("numTasks must be > 0");
+        if (numThreads <= 0)
+            throw new IllegalArgumentException("numThreads must be > 0");
+
+        this.totalPoints = totalPoints;
+        this.numTasks = numTasks;
+        this.numThreads = numThreads;
     }
 
+    public long getTotalPoints() {
+        return totalPoints;
+    }
+
+    public int getNumTasks() {
+        return numTasks;
+    }
+
+    public int getNumThreads() {
+        return numThreads;
+    }
+
+    @Override
+    public String toString() {
+        return "SimulationConfig{" +
+                "totalPoints=" + totalPoints +
+                ", numTasks=" + numTasks +
+                ", numThreads=" + numThreads +
+                '}';
+    }
 }
